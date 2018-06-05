a43fbe0c5d21d67c4127cec2a7584f342fd7f1d1
Ayush Raj <ayusofayush@gmail.com>
2018-06-05 13:52:12 +0530

    Update NoteMe.py

--- NoteMe.py
+++ NoteMe.py
@@ -121,7 +121,7 @@ class Notepad:
         # exit()
  
     def __showAbout(self):
-        showinfo("Notepad","Mrinal Verma")
+        showinfo("Notepad","This NotePad is designed by Ayush Raj using Tkinter.")
  
     def __openFile(self):
          
